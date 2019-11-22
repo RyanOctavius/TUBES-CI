e7a597c78dabf1fee08f222934a7bf219b5a7baf
RyanOctavius <ryanoctavius8899@gmail.com>
2019-11-22 17:11:28 +0700

    new

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,37 +1,219 @@
 <?php
-use Restserver \Libraries\REST_Controller ; 
-Class User extends REST_Controller{
-    public function __construct(){ 
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Methods: GET, OPTIONS, POST, DELETE");
-        header("Access-Control-Allow-Headers: Content-Type, Content- Length, Accept-Encoding");
-        parent::__construct(); 
-        $this->load->model('UserModel'); 
-        $this->load->library('form_validation');
+
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    require APPPATH . '/libraries/REST_Controller.php';
+
+
+    class User extends \Restserver\Libraries\REST_Controller 
+    {
+        public function __construct() 
+        {
+         header('Access-Control-Allow-Origin: *');
+      header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
+            parent::__construct();
+            $this->load->model('UserModel','User');
+            $this->load->helper('string');
+        }
+
+
+        public function index_get($id = null)
+        {
+            if ($id == null)
+                $data = $this->User->getAll();
+            else
+                $data = $this->User->getAll('id_user', $id);
+            
+            $this->response([
+                'status' => TRUE,
+                'message' => 'Success',
+                'data' => $data
+            ], \Restserver\Libraries\REST_Controller::HTTP_OK);
+        }
+
+
+        public function index_post() 
+        {
+            $id =  random_string('sha1');
+            $username = $this->post('username');
+            $email = $this->post('email');
+            $password = password_hash($this->post('password'), PASSWORD_DEFAULT);
+            $noTelp = $this->post('noTelp');
+            $role = $this->post('role');
+
+            $temp = $this->User->getAll('email', $email);
+
+            if ($temp != null) {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Email Sudah Terdaftar!',
+                    'data' => $temp
+                ], \Restserver\Libraries\REST_Controller::HTTP_BAD_REQUEST);
+            } else if ($username != null && $password != null &&$email != null && $noTelp != null &&
+                $role != null) {
+                    $data = [
+                        'id' =>  $id,
+                        'username' => $username,
+                        'email' => $email,
+                        'password' => $password,                        
+                        'noTelp' => $noTelp,
+                        'role' => $role,
+                        'status' => 0
+                    ];
+
+                    $config = [
+                        'mailtype'  => 'html',
+                        'charset'   => 'utf-8',
+                        'protocol'  => 'smtp',
+                        'smtp_host' => 'ssl://smtp.gmail.com',
+                        'smtp_user' => 'tumbalios1@gmail.com', 
+                        'smtp_pass' => 'terserah123',     
+                        'smtp_port' => 465,
+                        'crlf'      => "\r\n",
+                        'newline'   => "\r\n"
+                    ];
+
+                    $this->load->library('email', $config);
+                    $this->email->from('tumbalios1@gmail.com', 'PAW CLUB');
+                    $this->email->to($email);
+                    $this->email->subject('Verifikasi Email PAW CLUB');
+                    $this->email->message("<p>Setelah melakukan pendaftaran akun user <strong>PAW CLUB</strong>, lakukan <b>AKTIVASI</b> akun Anda dengan mengklik tautan berikut : </p><br>".base_url()."User/verifikasi/?email=$email&kode=$password"); 
+                    
+                    if ($this->email->send()) {
+                        if ($this->User->createUser($data) > 0) {
+                            $this->response([
+                                'status' => TRUE,
+                                'message' => 'Berhasil',
+                                'data' => $data
+                            ], \Restserver\Libraries\REST_Controller::HTTP_OK);
+                        } else {
+                            $this->response([
+                                'status' => FALSE,
+                                'message' => 'Gagal!'
+                            ], \Restserver\Libraries\REST_Controller::HTTP_BAD_REQUEST);
+                        }
+                    } else {
+                        $this->response([
+                            'status' => FALSE,
+                            'message' => 'Koneksi Error!'
+                        ], \Restserver\Libraries\REST_Controller::HTTP_BAD_REQUEST);
+                    }
+            } else {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Inputan Tidak Ditemukan!'
+                ], \Restserver\Libraries\REST_Controller::HTTP_NOT_FOUND);
+            }
+        }
+
+
+        public function index_delete() 
+        {
+            $id = $this->delete('id');
+
+            if ($id != null) {
+                $data = $this->User->getAll('id_user', $id);
+
+                if ($this->User->deleteUser($id) > 0) {
+                    $this->response([
+                        'status' => TRUE,
+                        'message' => 'Berhasil',
+                        'data' => $data
+                    ], \Restserver\Libraries\REST_Controller::HTTP_OK);
+                } else {
+                    $this->response([
+                        'status' => FALSE,
+                        'message' => 'ID Tidak Ditemukan!'
+                    ], \Restserver\Libraries\REST_Controller::HTTP_BAD_REQUEST);
+                }
+            } else {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Inputan Tidak Ditemukan!'
+                ], \Restserver\Libraries\REST_Controller::HTTP_NOT_FOUND);
+            }
+        }
+
+
+        public function edit_post()
+        {
+            $id =  $this->post('id');
+            $username = $this->post('username');
+            $email = $this->post('email');
+            $noTelp = $this->post('noTelp');
+            $role = $this->post('role');
+
+            if ($nama != null && $email != null && $nohp != null &&
+                $gender != null && $id != null) {
+                    $data = [
+                        'username' => $username,
+                        'email' => $email,
+                        'noTelp' => $noTelp,
+                        'role' => $role,
+                    ];
+
+                    if ($this->User->updateUser($data, $id) > 0) {
+                        $this->response([
+                            'status' => TRUE,
+                            'message' => 'Berhasil',
+                            'data' => $data
+                        ], \Restserver\Libraries\REST_Controller::HTTP_OK);
+                    } else {
+                        $this->response([
+                            'status' => FALSE,
+                            'message' => 'ID Tidak Ditemukan!'
+                        ], \Restserver\Libraries\REST_Controller::HTTP_BAD_REQUEST);
+                    }
+            } else {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Inputan Tidak Ditemukan!'
+                ], \Restserver\Libraries\REST_Controller::HTTP_NOT_FOUND);
+            }
+        }
+
+
+        public function login_post() {
+            $email = $this->post('email');
+            $password = $this->post('password');
+
+            if ($this->User->loginUser($email, $password) > 0) {
+                $this->response([
+                    'status' => TRUE,
+                    'message' => 'Berhasil',
+                    'data' => $this->User->getAll('email', $email)
+                ], \Restserver\Libraries\REST_Controller::HTTP_OK);
+            } else if ($this->User->loginUser($email, $password) == -1) {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Email Tidak Terdaftar!'
+                ], \Restserver\Libraries\REST_Controller::HTTP_BAD_REQUEST);
+            } else {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Email / Kata Sandi Salah!'
+                ], \Restserver\Libraries\REST_Controller::HTTP_NOT_FOUND);
+            }
+        }
+
+
+        public function verifikasi_get() {
+            $email = $this->get('email');
+            $kode = $this->get('kode');
+
+            if ($this->User->verifikasiEmail($email, $kode) > 0) {
+                $this->response([
+                    'status' => TRUE,
+                    'message' => 'Berhasil'
+                ], \Restserver\Libraries\REST_Controller::HTTP_OK);
+            } else {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Gagal!'
+                ], \Restserver\Libraries\REST_Controller::HTTP_NOT_FOUND);
+            }
+        }
     }
 
-    public function index_get($username = null){
-        $response = $this->UserModel->getInfo($username);
-    }
 
-    public function index_post($id = null){
-        //
-    }
-
-    public function index_delete($id = null){ 
-        //tidak ada delete
-    }
-
-
-    public function returnData($msg,$error){
-        $response['error']=$error; 
-        $response['message']=$msg;
-        return $this->response($response);
-    } 
-}
-
-Class UserData{
-    public $name;
-    public $password;
-    public $email;
-}
+?>
--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -1,104 +1,77 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class UserModel extends CI_Model {
-    private $table = 'user';
-    public $id; 
-    public $username; 
-    public $email; 
-    public $password;
-    public $noTelp; 
-    public $role;
-    public $rule = [
-        [ 
-            'field' => 'username',  
-            'label' => 'username',
-            'rules' => 'required'
-        ]
-    ];
-    public function Rules() { return $this->rule; }
-    public function getAll() { 
-        return $this->db->get('data_user')->result();
-    }
+    class UserModel extends CI_Model {
 
-    public function store($request) {
-        $this->username = $request->username;
-        $this->email = $request->email;
-        $this->password = password_hash($request->password, PASSWORD_BCRYPT); 
-        $this->noTelp = $request->noTelp;
-        $this->role = $request->role;
-        $query =  "SELECT * FROM user WHERE username = ?" ;
-        $result = $this->db->query($query, $this->username);
-        if($result->num_rows() == 0){
-            if($this->db->insert($this->table, $this)){
-                return ['msg'=>'Berhasil','error'=>false];
-            }
-            return ['msg'=>'Gagal','error'=>true]; 
+        public function getAll($key = null, $value = null) {
+            if ($value == null || $key == null)
+                return $this->db->get('user')->result_array();
+            else
+                return $this->db->get_where('user', [$key => $value])->result_array();
         }
-        else{
-            return ['msg'=>'Sudah ada','error'=>true];
-        }
-    }
 
-    public function check_login($request) {
-        $this->username = $request->username;
-        $this->password =$request->password;
-        $query = "SELECT * FROM user WHERE username = ? LIMIT 1" ;
-        $result = $this->db->query($query, $this->username);
-        if($result->num_rows() != 0)
-        {
-            $hash = $result->row()->password;
-            if(password_verify($this->password, $hash)){
-                
-                return ['msg'=>'Berhasil','error'=>false]; 
-            }
-            else{
-                return ['msg'=>'Password salah','error'=>true];
+
+        public function createUser($data) {
+            $this->db->insert('user', $data);
+            return $this->db->affected_rows();
+        }
+
+
+        public function deleteUser($id) {
+            $this->db->delete('user', ['id_user' => $id]);
+            return $this->db->affected_rows();
+        }
+
+
+        public function updateUser($data, $id) {
+            $user = array(
+                'nama' => $data['nama'],
+                'email' => $data['email'],
+                'noTelp' => $data['noTelp'],
+                'role' => $data['role'],
+            );
+
+            $this->db->where('id_user', $id);
+            $this->db->update('user', $user);
+            return $this->db->affected_rows();
+        }
+
+
+        public function loginUser($email, $password) {
+            $data = $this->db->get_where('user', ['email' => $email])->result_array();
+
+            if ($data == null) 
+                return -1;
+            else {
+                if ($data[0]['status'] == 0)
+                    return -1;
+                else {
+                    if (password_verify($password, $data[0]['password']))
+                        return 1;
+                    else
+                        return 0;
+                }
             }
         }
-        else{
-            return ['msg'=>'Username tidak ada','error'=>true];
-        }
-    }
-       
-    
-    public function update($request,$username) {
-        $updateData = [
-        'email' => $request->email, 
-        'username' =>$request->username,
-        'password' => password_hash($request->password, PASSWORD_BCRYPT),
-        'noTelp' =>$request->noTelp,
-        'role' => $request->role,
-        ]; 
 
-        $query = "SELECT * FROM user WHERE username = ? LIMIT 1" ;
-        $result = $this->db->query($query, $username);
-        if($result->num_rows() == 0)
-        {
-            $query = "SELECT * FROM user WHERE email = ? LIMIT 1" ;
-            $result = $this->db->query($query, $request->email);
-            if($result->num_rows()==0)
-            {
-                $this->db->where('username',$username)->update($this->table, $updateData);
-                return ['msg'=>'Berhasil','error'=>false];
-            }
-            else{
-                return ['msg'=>'Email sudah ada','error'=>true];
+
+        public function verifikasiEmail($email, $kode) {
+            $data = $this->db->get_where('user', ['email' => $email])->result_array();
+
+            if ($data == null) 
+                return 0;
+            else {
+                if ($kode === $data[0]['password']) {
+                    $this->db->set('status', 1, FALSE);
+                    $this->db->where('email', $email);
+                    $this->db->update('user');
+
+                    return $this->db->affected_rows();
+                }
+                else
+                    return 0;
             }
         }
-        else{
-            return ['msg'=>'Username sudah ada','error'=>true];
-        }
-        
+
     }
 
-    public function destroy($id){
-    if (empty($this->db->select('*')->where(array('id' => $id))->get($this->table)->row())) 
-        return ['msg'=>'Id tidak ditemukan','error'=>true];
-    if($this->db->delete($this->table, array('id' => $id))){ 
-        return ['msg'=>'Berhasil','error'=>false];
-    }
-    return ['msg'=>'Gagal','error'=>true]; 
-    }
-}
 ?>
